05128da1a754482090aa2ecb5f6778b65ad29ba6
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-10-20 20:52:20 +0200

    all green vector, stack, map, set everything is good

--- 1-Testors/testor1/srcs/main_tester_vector.cpp
+++ 1-Testors/testor1/srcs/main_tester_vector.cpp
@@ -6,7 +6,7 @@
 /*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 16:23:15 by bvalette          #+#    #+#             */
-/*   Updated: 2021/10/20 20:28:48 by epfennig         ###   ########.fr       */
+/*   Updated: 2021/10/20 20:44:52 by epfennig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,5 @@ main( void )	{
 		throw std::exception();
 		return (1);
 	}
-
-	while (1) ;
 	return (0);
 }
